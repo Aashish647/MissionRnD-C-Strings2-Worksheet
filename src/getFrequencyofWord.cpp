5f6d7feba551930eb67d796db91de3e2808addb4
aashish647 <aashish647@missionrnd.com>
2016-03-01 01:43:50 -0800

    Submit Commit

--- src/getFrequencyofWord.cpp
+++ src/getFrequencyofWord.cpp
@@ -13,10 +13,58 @@ Note: Dont modify original str or word,Just return count ,Spaces can also be par
 #include <stdlib.h>
 
 int count_word_in_str_way_1(char *str, char *word){
-	return 0;
+	
+	if ((str == NULL) || (word==NULL))
+	{
+		return NULL;
+	}
+	int i, j = 0, count = 0, temp;
+	//printf("\nsentence-%s word-%s\n", str, word);
+	for (i = 0, j = 0; str[i] != '\0'; i++)
+	{
+		//printf("\nstr[%d]-%c word[%d]-%c\n", i, str[i], j, word[j]);
+		if (str[i] == word[j])
+		{
+			temp = i;
+			while (word[j] != '\0')
+			{
+				if (str[temp] == word[j])
+				{
+					j += 1;
+					temp += 1;
+				}
+				else
+				{
+					break;
+				}
+			}
+			if (word[j] == '\0')
+			{
+				count += 1;
+				j = 0;
+			}
+		}
+		/*else
+		{
+		j = 0;
+		}
+		if (word[j] == '\0')
+		{
+		count += 1;
+		j = 0;
+		}*/
+	}
+	return count;
 }
 
+
 int count_word_int_str_way_2_recursion(char *str, char *word){
-	return 0;
+
+	if ((str == NULL) || (word == NULL))
+	{
+		return NULL;
+	}
+	static int i = 0, j = 0;
+	if (str[i++] == word[j++]);
 }
 
